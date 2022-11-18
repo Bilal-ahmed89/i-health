68bbb86f18c6eea3761da33918079f6ed4237c6d
Bilal-ahmed89 <ba1770490@gmail.com>
2022-11-18 22:55:41 +0500

    added some js functions

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+
+
+$(document).ready(function(){
+
+    $('#openBar').click(function(){
+        $('#sidebar').show();
+        
+    })
+
+    $('#closeBar').click(function(){
+        $('#sidebar').hide();
+       
+    })
+    
+    $('#moreText').click(function(){
+        $('#extraText').toggle();
+       
+        
+    })
+    $('#play').click(function(){
+        $('#video').show();
+        
+    })
+    $('#close').click(function(){
+        $('#video').hide();
+        
+    })
+    
+    }
+
+
+
+)
+
+
+
+
+    
